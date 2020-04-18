4208875b032b26b360a886b1d4bb275ee9811d74
Glavak <gau-gau@ya.ru>
2020-04-18 15:10:27 +0700

    Player mesh & movement

---
+++ Assets/CameraFollower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public Transform Target;
+    
+    private Vector3 velocity;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, Target.position, ref velocity, 0.25f);
+    }
+}
---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float MaxMoveSpeed = 8; 
+    
+    private CharacterController controllerComponent;
+    private Animator animatorComponent;
+    private Vector3 moveSpeed;
+
+    private float grabCooldown;
+    
+    private static readonly int GrabParam = Animator.StringToHash("grab");
+    private static readonly int WalkSpeedParam = Animator.StringToHash("walk speed");
+
+    private void Start()
+    {
+        animatorComponent = GetComponent<Animator>();
+        controllerComponent = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        UpdateWalk();
+
+        if (Input.GetKeyDown(KeyCode.Space)) Grab();
+        grabCooldown -= Time.deltaTime;
+    }
+
+    private void UpdateWalk()
+    {
+        float ySpeed = moveSpeed.y;
+        moveSpeed.y = 0;
+        
+        Vector3 target = MaxMoveSpeed * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        moveSpeed = Vector3.MoveTowards(moveSpeed, target, Time.deltaTime * 30);
+
+        animatorComponent.SetFloat(WalkSpeedParam, moveSpeed.magnitude);
+
+        if (moveSpeed.magnitude > 0.1f) transform.LookAt(transform.position + moveSpeed);
+
+        moveSpeed.y = ySpeed + Physics.gravity.y * Time.deltaTime;
+        controllerComponent.Move(moveSpeed * Time.deltaTime);
+    }
+
+    private void Grab()
+    {
+        if (grabCooldown > 0) return;
+        
+        animatorComponent.SetTrigger(GrabParam);
+        Debug.Log("Grab");
+
+        grabCooldown = 5f/6;
+    }
+
+    public void GrabAnimationCallback()
+    {
+        
+    }
+}
